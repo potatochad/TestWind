2c33d78112e53addc64b06bf6024b71ebfd5b44f
potatochad <163626408+potatochad@users.noreply.github.com>
2026-08-27 23:39:39 +0300

    Update BlockingLogic.kt

--- app/src/main/java/com/productivity/testwind/Screens/Web/BlockingLogic.kt
+++ app/src/main/java/com/productivity/testwind/Screens/Web/BlockingLogic.kt
@@ -26,7 +26,7 @@ import android.webkit.WebViewClient
 import android.graphics.Bitmap
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.activity.compose.*
-import com.productivity.wind.Imports.Utils.*
+import com.productivity.testwind.Imports.Utils.*
 import androidx.compose.foundation.lazy.*
 import androidx.compose.foundation.lazy.items
 import androidx.compose.ui.unit.*
