69a64fc7c4b42f42378bc8ac67294158f509de0b
potatochad <163626408+potatochad@users.noreply.github.com>
2026-08-27 13:26:30 +0300

    Add files via upload

---
+++ app/src/main/java/com/productivity/testwind/Imports/Utils/AppsAndDevice/Apps.kt
@@ -0,0 +1,232 @@
+package com.productivity.wind.Imports.Utils.AppsAndDevice            
+
+import com.productivity.wind.Imports.Utils.Log.*
+import com.productivity.wind.Imports.Utils.Generic_list.*
+import com.productivity.wind.Imports.Utils.Renames.*
+import com.productivity.wind.Imports.Utils.SaveData.*
+import com.productivity.wind.Imports.Utils.NavControl.*
+import com.productivity.wind.Imports.Utils.ToX.*
+import com.productivity.wind.Imports.Utils.String.*
+import android.annotation.SuppressLint
+import timber.log.Timber
+import java.text.*
+import android.app.usage.UsageStatsManager
+import androidx.compose.foundation.interaction.*
+import android.app.*
+import androidx.core.app.*
+import androidx.compose.ui.draw.*
+import androidx.compose.ui.unit.*
+import androidx.compose.material3.*
+import androidx.compose.foundation.text.*
+import androidx.compose.ui.text.input.*
+import androidx.compose.runtime.*
+import android.os.*
+import android.content.*
+import android.util.*
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.*
+import androidx.compose.ui.text.font.*
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import kotlinx.coroutines.*
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.KMutableProperty1
+import kotlin.reflect.jvm.isAccessible
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlin.reflect.*
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import android.net.Uri
+import android.widget.Toast
+import com.productivity.wind.*
+import java.util.UUID
+import java.lang.reflect.Type
+import kotlin.collections.*
+import android.content.*
+import java.lang.reflect.ParameterizedType
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.*
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.res.painterResource
+import android.content.Intent
+import java.time.LocalDate
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.productivity.wind.Screens.*
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.layout.*
+import android.graphics.drawable.Drawable
+import android.content.pm.*
+import com.productivity.wind.Imports.*
+import java.util.*
+import com.productivity.wind.R
+import kotlin.reflect.full.*
+import androidx.compose.ui.focus.*
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.ActivityResultLauncher
+import androidx.compose.foundation.gestures.*
+import androidx.compose.ui.input.pointer.*
+import java.io.*
+import androidx.compose.ui.geometry.*
+import androidx.compose.ui.text.style.*
+import androidx.compose.foundation.lazy.*
+import java.util.*
+import kotlin.concurrent.*
+import androidx.annotation.RequiresApi
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.core.view.*
+import kotlin.reflect.*
+import kotlin.reflect.jvm.*
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+import androidx.compose.foundation.text.selection.*
+import kotlin.system.*
+import androidx.navigation.*
+import android.webkit.*
+import org.jetbrains.kotlin.cli.jvm.K2JVMCompiler
+import org.jetbrains.kotlin.cli.common.ExitCode
+import com.productivity.wind.Imports.Utils.*
+import android.location.*
+import androidx.core.content.*
+import androidx.compose.ui.text.*
+import androidx.navigation.compose.*
+import android.util.*
+import com.google.android.gms.maps.model.*
+import com.google.android.gms.location.*
+import android.content.*
+import android.net.*
+import androidx.lifecycle.*
+import kotlinx.coroutines.flow.*
+import androidx.compose.ui.window.*
+import com.productivity.wind.Imports.UI_visible.*
+import android.os.Process.*
+import android.content.ClipData
+import android.app.usage.UsageEvents
+import android.content.ClipboardManager
+import androidx.compose.ui.graphics.toArgb
+import java.lang.ref.WeakReference
+import android.provider.Settings
+
+
+fun TodayAppUsage(packageName: Str): Int {
+    val end = System.currentTimeMillis()
+    val cal = Calendar.getInstance().apply {
+        set(Calendar.HOUR_OF_DAY, 0)
+        set(Calendar.MINUTE, 0)
+        set(Calendar.SECOND, 0)
+        set(Calendar.MILLISECOND, 0)
+    }
+    val start = cal.timeInMillis
+
+    val usm = App.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+
+    // Use INTERVAL_BEST and filter manually
+    val stats = usm.queryUsageStats(UsageStatsManager.INTERVAL_BEST, start, end)
+    val todayUsage = stats
+        .filter { it.packageName == packageName && it.lastTimeUsed >= start }
+        .sumOf { it.totalTimeInForeground }
+
+    return (todayUsage / 1000L).toInt().coerceAtLeast(0)
+}
+
+
+fun AppInfo.pkg(): Str {
+    return this.activityInfo?.packageName ?: ""
+}
+
+
+fun LastUsedApp(context: Context = App, lookBackMs: Long = 10_000): Str? {
+    val usm = context.getSystemService(UsageStatsManager::class.java)
+
+    val end = System.currentTimeMillis()
+    val start = end - lookBackMs
+
+    val events = usm.queryEvents(start, end)
+    val event = UsageEvents.Event()
+
+    var lastApp: String? = null
+
+    while (events.hasNextEvent()) {
+        events.getNextEvent(event)
+
+        if (event.eventType == UsageEvents.Event.MOVE_TO_FOREGROUND) {
+            lastApp = event.packageName
+        }
+    }
+
+    return lastApp
+}
+
+
+fun Str.pkg(): Str {
+    val pm = App.packageManager
+    return pm.getInstalledApplications(0)
+        .firstOrNull { pm.getApplicationLabel(it).toString().equals(this, ignoreCase = yes) }
+        ?.packageName ?: this
+}
+
+fun OpenApp(pkg: Str) {
+    val pm: PackageManager = App.packageManager
+    val launchIntent: Intent? = pm.getLaunchIntentForPackage(pkg)
+    if (launchIntent != null) {
+        App.startActivity(launchIntent)
+    } else {
+        Vlog("App not installed")
+    }
+}
+
+val ResolveInfo.name: Str
+    get() {
+        val pkg = this.activityInfo.packageName
+        return this.loadLabel(App.packageManager)?.toString() ?: pkg
+	}
+	
+fun AppIcon(packageName: Str?): Drawable? {
+    if (packageName.isNullOrBlank()) {
+        return App.getDrawable(android.R.drawable.sym_def_app_icon)
+    }
+
+    val pm = App.packageManager
+    return try {
+        val info = pm.getApplicationInfo(packageName, 0)
+        info.loadIcon(pm)
+    } catch (e: Exception) {
+        log("Bad package: $packageName")
+        App.getDrawable(android.R.drawable.sym_def_app_icon)
+    }
+}
+
