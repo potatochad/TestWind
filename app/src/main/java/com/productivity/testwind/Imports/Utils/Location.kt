a7073156a3af97ebccb834e4aaf087ed1318dff4
potatochad <163626408+potatochad@users.noreply.github.com>
2026-08-27 23:18:23 +0300

    Update Location.kt

--- app/src/main/java/com/productivity/testwind/Imports/Utils/Location.kt
+++ app/src/main/java/com/productivity/testwind/Imports/Utils/Location.kt
@@ -82,7 +82,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import com.productivity.wind.Screens.*
+import com.productivity.testwind.Screens.*
 import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.rememberTextMeasurer
@@ -120,7 +120,7 @@ import androidx.navigation.*
 import android.webkit.*
 import org.jetbrains.kotlin.cli.jvm.K2JVMCompiler
 import org.jetbrains.kotlin.cli.common.ExitCode
-import com.productivity.wind.Imports.Utils.*
+import com.productivity.testwind.Imports.Utils.*
 import android.location.*
 import androidx.core.content.*
 import androidx.compose.ui.text.*
