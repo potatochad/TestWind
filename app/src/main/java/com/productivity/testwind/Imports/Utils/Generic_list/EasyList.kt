1f3a15f56d75cadff62b5ffbc6e8eee57ff89d6f
potatochad <163626408+potatochad@users.noreply.github.com>
2026-08-27 17:52:29 +0300

    Update EasyList.kt

--- app/src/main/java/com/productivity/testwind/Imports/Utils/Generic_list/EasyList.kt
+++ app/src/main/java/com/productivity/testwind/Imports/Utils/Generic_list/EasyList.kt
@@ -1,12 +1,12 @@
-package com.productivity.wind.Imports.Utils.Generic_list
+package com.productivity.testwind.Imports.Utils.Generic_list
 
-import com.productivity.wind.Imports.Utils.Log.*
-import com.productivity.wind.Imports.Utils.Renames.*
-import com.productivity.wind.Imports.Utils.SaveData.*
-import com.productivity.wind.Imports.Utils.AppsAndDevice.*
-import com.productivity.wind.Imports.Utils.NavControl.*
-import com.productivity.wind.Imports.Utils.ToX.*
-import com.productivity.wind.Imports.Utils.String.*
+import com.productivity.testwind.Imports.Utils.Log.*
+import com.productivity.testwind.Imports.Utils.Renames.*
+import com.productivity.testwind.Imports.Utils.SaveData.*
+import com.productivity.testwind.Imports.Utils.AppsAndDevice.*
+import com.productivity.testwind.Imports.Utils.NavControl.*
+import com.productivity.testwind.Imports.Utils.ToX.*
+import com.productivity.testwind.Imports.Utils.String.*
 import android.annotation.SuppressLint
 import timber.log.Timber
 import java.text.*
@@ -46,7 +46,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import android.net.Uri
 import android.widget.Toast
-import com.productivity.wind.*
+import com.productivity.testwind.*
 import java.util.UUID
 import java.lang.reflect.Type
 import kotlin.collections.*
@@ -90,7 +90,7 @@ import android.graphics.drawable.Drawable
 import android.content.pm.*
 import com.productivity.wind.Imports.*
 import java.util.*
-import com.productivity.wind.R
+import com.productivity.testwind.R
 import kotlin.reflect.full.*
 import androidx.compose.ui.focus.*
 import androidx.activity.result.contract.ActivityResultContract
@@ -138,7 +138,7 @@ import org.json.JSONObject
 import com.productivity.wind.Imports.UI_visible.*
 import kotlinx.coroutines.flow.*
 import kotlinx.serialization.builtins.ListSerializer
-import com.productivity.wind.Imports.Utils.Log.logTimer
+import com.productivity.testwind.Imports.Utils.Log.logTimer
 
 
 abstract class EasyListExtra<T>(
