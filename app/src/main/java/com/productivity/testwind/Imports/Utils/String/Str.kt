c999bdd265e83357172b2f069ecbf7bf5b3e81bb
potatochad <163626408+potatochad@users.noreply.github.com>
2026-08-27 17:36:36 +0300

    Update Str.kt

--- app/src/main/java/com/productivity/testwind/Imports/Utils/String/Str.kt
+++ app/src/main/java/com/productivity/testwind/Imports/Utils/String/Str.kt
@@ -1,12 +1,12 @@
-package com.productivity.wind.Imports.Utils.String
+package com.productivity.testwind.Imports.Utils.String
 
-import com.productivity.wind.Imports.Utils.Log.*
-import com.productivity.wind.Imports.Utils.Generic_list.*
-import com.productivity.wind.Imports.Utils.Renames.*
-import com.productivity.wind.Imports.Utils.SaveData.*
-import com.productivity.wind.Imports.Utils.AppsAndDevice.*
-import com.productivity.wind.Imports.Utils.NavControl.*
-import com.productivity.wind.Imports.Utils.ToX.*
+import com.productivity.testwind.Imports.Utils.Log.*
+import com.productivity.testwind.Imports.Utils.Generic_list.*
+import com.productivity.testwind.Imports.Utils.Renames.*
+import com.productivity.testwind.Imports.Utils.SaveData.*
+import com.productivity.testwind.Imports.Utils.AppsAndDevice.*
+import com.productivity.testwind.Imports.Utils.NavControl.*
+import com.productivity.testwind.Imports.Utils.ToX.*
 import android.annotation.SuppressLint
 import timber.log.Timber
 import java.text.*
@@ -46,7 +46,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import android.net.Uri
 import android.widget.Toast
-import com.productivity.wind.*
+import com.productivity.testwind.*
 import java.util.UUID
 import java.lang.reflect.Type
 import kotlin.collections.*
@@ -81,16 +81,16 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import com.productivity.wind.Screens.*
+import com.productivity.testwind.Screens.*
 import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.layout.*
 import android.graphics.drawable.Drawable
 import android.content.pm.*
-import com.productivity.wind.Imports.*
+import com.productivity.testwind.Imports.*
 import java.util.*
-import com.productivity.wind.R
+import com.productivity.testwind.R
 import kotlin.reflect.full.*
 import androidx.compose.ui.focus.*
 import androidx.activity.result.contract.ActivityResultContract
@@ -119,7 +119,7 @@ import androidx.navigation.*
 import android.webkit.*
 import org.jetbrains.kotlin.cli.jvm.K2JVMCompiler
 import org.jetbrains.kotlin.cli.common.ExitCode
-import com.productivity.wind.Imports.Utils.*
+import com.productivity.testwind.Imports.Utils.*
 import android.location.*
 import androidx.core.content.*
 import androidx.compose.ui.text.*
@@ -131,8 +131,8 @@ import android.content.*
 import android.net.*
 import androidx.lifecycle.*
 import kotlinx.coroutines.flow.*
-import com.productivity.wind.Imports.UI_visible.*
-import com.productivity.wind.Imports.Utils.*
+import com.productivity.testwind.Imports.UI_visible.*
+import com.productivity.testwind.Imports.Utils.*
 
 
 
